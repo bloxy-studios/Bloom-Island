beecebcbc1fde78026ad3e7c19c0ff188eb79863
bloxy-studios <abdulsdevworkspace@gmail.com>
2026-07-19 05:10:36 -0400

    Initial Commit

---
+++ Bloom/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Bloom
+//
+//  Created by Abdul Karim Ali on 7/19/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BloomTests/BloomTests.swift
@@ -0,0 +1,19 @@
+//
+//  BloomTests.swift
+//  BloomTests
+//
+//  Created by Abdul Karim Ali on 7/19/26.
+//
+
+import Testing
+@testable import Bloom
+
+struct BloomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
