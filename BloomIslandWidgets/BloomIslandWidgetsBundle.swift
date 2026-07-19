29daa23998120f5626b335c2afd7de217884095c
bloxy-studios <abdulsdevworkspace@gmail.com>
2026-07-19 12:02:34 -0400

    feat: Phase C local updates

---
+++ BloomIslandWidgets/BloomIslandWidgetsBundle.swift
@@ -0,0 +1,18 @@
+//
+//  BloomIslandWidgetsBundle.swift
+//  BloomIslandWidgets
+//
+//  Created by Abdul Karim Ali on 7/19/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BloomIslandWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        BloomIslandWidgets()
+        BloomIslandWidgetsControl()
+        BloomIslandWidgetsLiveActivity()
+    }
+}
